c829b06b9f280548744ba9ea8057f04305f41322
Manash <manashmndl@gmail.com>
2019-02-22 20:03:26 +0600

    async crawler

--- crawler.py
+++ crawler.py
@@ -21,18 +21,31 @@ blaze""".split('\n')
 news = MongoClient(connect=False)['dailystar']['news']
 
 sd = "01-09-2007"
-ed = "01-09-2008"
+ed = "31-12-2007"
 DATE_FORMATTER = "%d-%m-%Y"
 
 start_date = datetime.datetime.strptime(sd, DATE_FORMATTER)
 end_date = datetime.datetime.strptime(ed, DATE_FORMATTER)
 
-current_date = start_date
+def url_generator(start_date, end_date):
+    current_date = start_date
+    urls = []
+    for day in range((end_date - start_date).days):
+        urls.append(URL + current_date.strftime(DATE_FORMATTER))
+        current_date += datetime.timedelta(days=1)
+    return urls
 
-for day in range((end_date - current_date).days):
-    
-    get_current_date_page = requests.get(URL + current_date.strftime(DATE_FORMATTER))
+urls = url_generator(start_date, end_date)
 
-    print(get_current_date_page.url)
+import asyncio
 
-    current_date += datetime.timedelta(days=1)
+async def fetch_page(url):
+    url = requests.get(url)
+    print(url.url)
+
+async def main():
+    tasks = [asyncio.ensure_future(fetch_page(url)) for url in urls]
+    await asyncio.wait(tasks)
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(asyncio.ensure_future(main()))
